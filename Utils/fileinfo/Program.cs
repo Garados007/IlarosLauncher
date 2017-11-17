34ead7a5c5fc5e8731865aad642674b11839ef3c
Max Brauer <ma.brauer@live.de>
2017-11-17 14:05:20 +0100

    Ein Programm wurde hinzugefügt, welches die Versionsinformationen von verschiedenen Programmen und DLLs auslesen kann.

---
+++ Utils/fileinfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace fileinfo
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1) return 1;
+            var file = args[0];
+            Assembly ass;
+            try { ass = Assembly.LoadFrom(file); }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return 1;
+            }
+            var version = ass.GetName().Version;
+            Console.WriteLine(version.ToString());
+            return 0;
+        }
+    }
+}
---
+++ Utils/fileinfo/post-process.bat
@@ -0,0 +1,14 @@
+﻿
+REM Kopiert alle relevanten Dateien in den Ausgabeordner
+
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set curDir=%CD%
+set tarDir=%CD%\..\..\..\bin\%1\Utils
+
+mkdir "%tarDir%"
+
+rem Dateien kopieren
+
+copy /Y "%curDir%\fileinfo.exe" "%tarDir%\fileinfo.exe"
