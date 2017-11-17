4c695b94662111a0c92e0d231c6aa02e860f54a5
Max Brauer <ma.brauer@live.de>
2017-11-17 15:44:41 +0100

    Die Update Anwendung zum Download des eigentlichen Updaters wurde hinzugefügt.

---
+++ Install/IlarosLauncher.Update/DisplayDownload.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace IlarosLauncher.Update
+{
+    public class DisplayDownload : Form
+    {
+        private System.ComponentModel.IContainer components;
+        private ProgressBar progressBar1;
+        string tempPath = "%TEMP%\\IlarosLauncher";
+
+        public DisplayDownload()
+        {
+            getTempPath();
+            if (!Application.StartupPath.StartsWith(tempPath))
+            {
+                System.IO.File.Copy(Application.ExecutablePath, tempPath + "\\IlarosLauncher.Update.exe");
+                System.Diagnostics.Process.Start(tempPath + "\\IlarosLauncher.Update.exe");
+                return;
+            }
+            InitializeComponent();
+            Load += DisplayDownload_Load;
+        }
+
+        void getTempPath()
+        {
+            var key = Registry.CurrentUser.OpenSubKey("Software")?.OpenSubKey("IlarosLauncher");
+            if (key == null) return;
+            var path = key.GetValue("TempPath", tempPath);
+            tempPath = path.ToString();
+        }
+
+        private void DisplayDownload_Load(object sender, EventArgs e)
+        {
+            var wc = new System.Net.WebClient();
+            wc.DownloadProgressChanged += (s, ev) =>
+            {
+                progressBar1.Value = ev.ProgressPercentage;
+            };
+            wc.DownloadFileCompleted += (s, ev) =>
+            {
+                System.Diagnostics.Process.Start(tempPath+ "\\IlarosLauncher.UpdateClient.exe");
+            };
+            wc.DownloadFileAsync(new Uri(DownloadSetting.ServerUrl + "?mode=updater"), 
+                tempPath + "\\IlarosLauncher.UpdateClient.exe");
+        }
+
+        private void InitializeComponent()
+        {
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Dock = DockStyle.Fill;
+            progressBar1.Location = new System.Drawing.Point(0, 0);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Size = new System.Drawing.Size(275, 29);
+            progressBar1.TabIndex = 0;
+            // 
+            // DisplayDownload
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(275, 29);
+            ControlBox = false;
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "DisplayDownload";
+            Text = "Ilaros Launcher - Updater";
+            ResumeLayout(false);
+
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DisplayDownload());
+        }
+    }
+}
---
+++ Install/IlarosLauncher.Update/DownloadSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace IlarosLauncher.Update
+{
+    public class DownloadSetting
+    {
+        public const string ServerType = "ApachePHPV1";
+
+        public const string ServerUrl = @"";
+    }
+}
