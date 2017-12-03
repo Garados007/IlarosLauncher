f6ec020cd605d6b3f3e01e5c80b3060f1dca028c
Max Brauer <ma.brauer@live.de>
2017-12-03 15:27:40 +0100

    Das Launcherfenster ist nun standardmäßig größer.

--- IlarosLauncher/Form1.Designer.cs
+++ IlarosLauncher/Form1.Designer.cs
@@ -38,14 +38,14 @@
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(646, 442);
+            this.webBrowser1.Size = new System.Drawing.Size(927, 600);
             this.webBrowser1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 442);
+            this.ClientSize = new System.Drawing.Size(927, 600);
             this.Controls.Add(this.webBrowser1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
