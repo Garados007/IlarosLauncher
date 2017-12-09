9941f60d2de8d75155f8eb40654764054bb09862
Max Brauer <ma.brauer@live.de>
2017-12-09 15:19:45 +0100

    Launcher Modul API für den Hauptzugriff wurde hinzugefügt

---
+++ IlarosLauncher/Modules/ModuleWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jint;
+
+namespace IlarosLauncher.Modules
+{
+    class ModuleWorker
+    {
+        public Engine Engine { get; private set; }
+
+        public void LoadCode(string path)
+        {
+
+        }
+    }
+}
