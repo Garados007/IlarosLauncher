1c7aa82cd5d65143ef8a8b11554ed763f51778a4
Max Brauer <ma.brauer@live.de>
2017-12-09 19:39:23 +0100

    Automatischer Voter für wow-portal.eu hinzugefügt (Name noch nicht festgelegt) (#12)

---
+++ Content/Source/Modules/vote wow-portal.eu/main.js
@@ -0,0 +1,22 @@
+﻿// wow-portal.eu automatic voter
+
+var user = "betauser";
+var error = false;
+try {
+    var code = $.Net.Get("http://wow-portal.eu/toplist.php?mode=in&id=30302");
+    if ($.Net.Error == null) {
+        var ind = code.indexOf("include.php?mode=votetoplist&id=30302&randomdigit=");
+        var ind2 = code.indexOf("\\", ind);
+        code = code.substring(ind, ind2);
+        $.Net.Get("http://wow-portal.eu/" + code);
+    }
+    if ($.Net.Error != null) {
+        error = true;
+        $.Log($.Net.Error);
+    }
+}
+catch (e) {
+    error = true;
+    $.Log(e);
+}
+$.Event("vote", "wow-portal.eu", !error);
