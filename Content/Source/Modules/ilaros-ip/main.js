44e0de18443571de0b9e087d11db47372b9cb609
Max Brauer <ma.brauer@live.de>
2017-12-09 18:44:59 +0100

    IP vom Spielserver wird nun herausgesucht (#10)

---
+++ Content/Source/Modules/ilaros-ip/main.js
@@ -0,0 +1,18 @@
+﻿var ip = $.Net.Get("http://www.wow.ilaros.de/ip.txt");
+if ($.Net.Error != null) {
+    $.Props.GameServerIP = "";
+    $.Log("Webseite offline");
+}
+else {
+    $.Log("IP: " + ip /*+ " (Ping: " + $.Net.Ping(ip) + "ms)"*/);
+    var checkup = $.Net.Get("http://" + ip + "/");
+    if ($.Net.Error == null) {
+        $.Props.GameServerIP = ip;
+        $.Log("Server online");
+    }
+    else {
+        $.Props.GameServerIP = "";
+        $.Log("Server offline");
+        $.Log($.Net.Error);
+    }
+}
