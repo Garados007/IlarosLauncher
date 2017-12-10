7e748bbb64d1c1ec9ff96464b3ece604d9f6f38c
Max Brauer <ma.brauer@live.de>
2017-12-10 17:50:28 +0100

    Urls können nun mit dem Browser geöffnet werden.

---
+++ IlarosLauncher/Services/RunUrlService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaxLib.Net.Webserver;
+
+namespace IlarosLauncher.Services
+{
+    class RunUrlService : WebService
+    {
+        public RunUrlService() : base(WebServiceType.PreCreateDocument)
+        {
+        }
+
+        public override bool CanWorkWith(WebProgressTask task)
+        {
+            return task.Document.RequestHeader.Location.IsUrl(new[] { "run-url" }) &&
+                task.Document.RequestHeader.Location.GetParameter.ContainsKey("url");
+        }
+
+        public override void ProgressTask(WebProgressTask task)
+        {
+            var url = task.Document.RequestHeader.Location.GetParameter["url"];
+            if (!url.StartsWith("http://") || !url.StartsWith("https://")) return;
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
