df76ea6c9c10dbe87458a99ecdd08dbb8247cc09
Max Brauer <ma.brauer@live.de>
2017-12-16 01:41:10 +0100

    der aktuelle Benutzername wird nun in die Konfiguration des WoW Client geschrieben, bevor  dieser gestartet wird. (close #17)

--- IlarosLauncher/Services/RunWowService.cs
+++ IlarosLauncher/Services/RunWowService.cs
@@ -23,14 +23,41 @@ namespace IlarosLauncher.Services
         {
             var wow = Server.UserSettings.UserSettings["wow"].Options.GetString("path", null);
             if (wow == null || string.IsNullOrEmpty(Server.GameServerIP) || !File.Exists(wow + "\\Wow.exe")) return;
+            //clear cache
             var cache = new DirectoryInfo(wow + "\\Cache");
             if (cache.Exists)
             {
                 foreach (var f in cache.EnumerateFiles()) f.Delete();
                 foreach (var d in cache.EnumerateDirectories()) d.Delete(true);
             }
+            //set ip
             if (!Directory.Exists(wow + @"\Data\deDE")) return;
             File.WriteAllText(wow + @"\Data\deDE\realmlist.wtf", "set realmlist " + Server.GameServerIP);
+            //set username
+            var g = Server.UserSettings.UserSettings["names"];
+            var ind = g?.Options.GetInt32("currentId", -1) ?? -1;
+            var name = g?.Options.GetString("name[" + ind.ToString() + "]", null);
+            var l = new List<string>();
+            try
+            {
+                if (name != null)
+                    using (var f = new FileStream(wow + "\\WTF\\Config.wtf", FileMode.OpenOrCreate))
+                    using (var r = new StreamReader(f))
+                    using (var w = new StreamWriter(f))
+                    {
+                        while (!r.EndOfStream)
+                        {
+                            var line = r.ReadLine();
+                            if (!line.StartsWith("SET accountName")) l.Add(line);
+                        }
+                        l.Add("SET accountName \"" + name + "\"");
+                        f.SetLength(0);
+                        f.Position = 0;
+                        for (int i = 0; i < l.Count; ++i) w.WriteLine(l[i]);
+                    }
+            }
+            catch { }
+            //start
             System.Diagnostics.Process.Start(wow + "\\Wow.exe");
         }
     }
