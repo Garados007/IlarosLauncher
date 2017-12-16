3a043a853c2f22a33d0dc51fed6d67713ac081da
Max Brauer <ma.brauer@live.de>
2017-12-16 01:51:05 +0100

    Der Server-Log des Clients wird nun unter /server-log/ angeboten.

---
+++ IlarosLauncher/Services/ServerLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaxLib.Net.Webserver;
+
+namespace IlarosLauncher.Services
+{
+    class ServerLog : WebService
+    {
+        public ServerLog() : base(WebServiceType.PreCreateDocument)
+        {
+        }
+
+        public override bool CanWorkWith(WebProgressTask task)
+        {
+            return task.Document.RequestHeader.Location.StartsUrlWith(new[] { "server-log" });
+        }
+
+        public override void ProgressTask(WebProgressTask task)
+        {
+            var text = new StringBuilder();
+            var log = WebServerInfo.Information.ToArray();
+            for (int i = 0; i < log.Length; ++i)
+            {
+                text.AppendLine(log[i].ToString().Replace("<", "&lt;").Replace(">", "&gt;") + "<br/>");
+            }
+            task.Document.DataSources.Add(new HttpStringDataSource(text.ToString())
+            {
+                MimeType = MimeTypes.TextPlain,
+                TextEncoding = "utf-8",
+                TransferCompleteData = true
+            });
+            task.Document.ResponseHeader.StatusCode = HttpStateCode.OK;
+            task.Document.PrimaryEncoding = "utf-8";
+        }
+    }
+}
